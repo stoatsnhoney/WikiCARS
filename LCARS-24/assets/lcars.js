c2a3a43f078e8fff0430295dd6765049c8226454
Jay <mailer_wombats.0u@icloud.com>
2025-03-20 09:34:08 -0700

    LCARS import

---
+++ LCARS-24/assets/lcars.js
@@ -0,0 +1,14 @@
+document.addEventListener("touchstart", function() {},false);
+let mybutton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
